f38fe927494eef9e22ccdf8b60fa132a136f5c4e
unknown <jl23868@an098063-ct103v.nvstu.edu>
2015-02-24 15:39:25 -0500

    Add Algorithm

---
+++ Algorithm.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Algorithm{
+   public static void main(String [] args){
+   Scanner keyboard = new Scanner(System.in);
+
+   String username, password;
+   String a = "CSC200";
+   String b = "JL23868";
+   
+   System.out.println("Enter the username");
+   username = keyboard.next();
+   System.out.println("Enter the password");
+   password = keyboard.next();
+   
+   if ((username.equals(a))&&(password.equals(b))){
+   System.out.println("Welcome, " + password);
+   } else {
+   System.out.println("Wrong username and password");
+   }
+
+   }
+}
+
